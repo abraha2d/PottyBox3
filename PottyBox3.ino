ade126b96e5d2c1759e8962e2a1133fea71779eb
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-06-29 23:13:08 -0400

    Dynamically detect sensor 2, set ROI to minimum

--- PottyBox3.ino
+++ PottyBox3.ino
@@ -8,14 +8,13 @@
 
 #define SENSOR_1_SHUTDOWN_PIN 2
 
-SFEVL53L1X distanceSensor1(Wire1);  // First sensor, with shutdown pin connected
-SFEVL53L1X distanceSensor2(Wire1);  // Second sensor
+SFEVL53L1X sensor1(Wire1);  // First sensor, with shutdown pin connected
+SFEVL53L1X* sensor2 = nullptr;  // Second sensor (optional)
+
+bool sensor2Present = false;
 
 void setup(void)
 {
-
-  pinMode(SENSOR_1_SHUTDOWN_PIN, OUTPUT);
-  digitalWrite(SENSOR_1_SHUTDOWN_PIN, LOW);
 
   Wire1.begin();
 
@@ -26,56 +25,106 @@ void setup(void)
   Serial.println("│ By Kevin Abraham │");
   Serial.println("└──────────────────┘");
   Serial.println();
-  Serial.println("Get latest version at:");
+  Serial.println("Get the latest version at:");
   Serial.println("https://github.com/abraha2d/PottyBox3");
   Serial.println();
 
-  // Change sensor 2's I2C address
-  while(!distanceSensor2.checkBootState());
-  distanceSensor2.begin();
-  distanceSensor2.setI2CAddress(0x54);
-
-  // Bring sensor 1 online
-  pinMode(SENSOR_1_SHUTDOWN_PIN, INPUT);
-  while(!distanceSensor1.checkBootState());
-  distanceSensor1.begin();
-
-  Serial.println ("I2C scanner. Scanning...");
+  Serial.println("Scanning for sensors...");
   byte count = 0;
-  for (byte i = 1; i < 120; i++) {
+  for (byte i = 0x08; i < 0x78; i++) {
     Wire1.beginTransmission(i);
     if (Wire1.endTransmission () == 0) {
-      Serial.print("Found address: ");
-      Serial.print(i, DEC);
-      Serial.print(" (0x");
-      Serial.print(i, HEX);
-      Serial.println(")");
+      Serial.print("Found sensor at address 0x");
+      Serial.println(i, HEX);
       count++;
       delay(1);
     }
   }
   Serial.print("Done. Found ");
   Serial.print(count, DEC);
-  Serial.println(" device(s).");
+  Serial.println(" sensor(s).");
   Serial.println();
+
+  if (count == 1) {
+
+    // Shut down sensor 1
+    Serial.print("Shutting down sensor 1...               ");
+    pinMode(SENSOR_1_SHUTDOWN_PIN, OUTPUT);
+    digitalWrite(SENSOR_1_SHUTDOWN_PIN, LOW);
+    Serial.println("Done.");
+
+    // Initiate scan for more sensors
+    Serial.print("Looking for sensor 2...                 ");
+    count = 0;
+    for (byte i = 0x08; i < 0x78; i++) {
+      Wire1.beginTransmission(i);
+      if (Wire1.endTransmission () == 0) {
+        count++;
+        delay(1);
+      }
+    }
+
+    if (count == 1) {
+      sensor2 = new SFEVL53L1X(Wire1);
+      sensor2Present = true;
+      Serial.println("Done.");
+
+      // Initialize sensor 2
+      Serial.print("Initializing sensor 2...                ");
+      while(!sensor2->checkBootState());
+      sensor2->begin();
+      Serial.println("Done.");
+
+      // Change sensor 2's I2C address
+      Serial.print("Assigning address 0x2A to sensor 2...   ");
+      sensor2->setI2CAddress(0x54);
+      Serial.println("Done.");
+
+    } else {
+      Serial.println("Fail.");
+    }
+
+    // Bring sensor 1 online
+    Serial.print("Bringing sensor 1 back online...        ");
+    pinMode(SENSOR_1_SHUTDOWN_PIN, INPUT);
+    Serial.println("Done.");
+
+    // Initialize sensor 1
+    Serial.print("Initializing sensor 1...                ");
+    while(!sensor1.checkBootState());
+    sensor1.begin();
+    Serial.println("Done.");
+
+    Serial.println("Sensor initialization complete.");
+    Serial.println();
+
+  } else if (count == 2) {
+    sensor2 = new SFEVL53L1X(Wire1, 0x54);
+    sensor2Present = true;
+  }
+
+  sensor1.setROI(11, 11);
+  sensor1.startRanging();
+  if (sensor2) {
+    sensor2->setROI(11, 11);
+    sensor2->startRanging();
+  }
 
 }
 
 void loop(void)
 {
-  distanceSensor1.startRanging();
-  int distance1 = distanceSensor1.getDistance();
-  distanceSensor1.stopRanging();
+  Serial.print("'");
+  Serial.print(sensor1.getRangeStatus());
+  Serial.print("' Distance (in): ");
+  Serial.print(sensor1.getDistance() * 0.0393701, 2);
 
-  Serial.print("\tDistance (in): ");
-  Serial.print(distance1 * 0.0393701, 2);
-
-  distanceSensor2.startRanging();
-  int distance2 = distanceSensor2.getDistance();
-  distanceSensor2.stopRanging();
-
-  Serial.print("\tDistance (in): ");
-  Serial.print(distance2 * 0.0393701, 2);
+  if (sensor2) {
+    Serial.print("\t'");
+    Serial.print(sensor2->getRangeStatus());
+    Serial.print("' Distance (in): ");
+    Serial.print(sensor2->getDistance() * 0.0393701, 2);
+  }
 
   Serial.println();
   delay(100);
